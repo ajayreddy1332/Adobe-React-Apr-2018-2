f8d52130eba8ccf34b862ea731e15bc702ec28e1
Magesh <tkmagesh77@gmail.com>
2018-04-25 16:27:00 +0530

    middleware introduction with logger middleware

--- 06-bug-tracker-app/src/store/index.js
+++ 06-bug-tracker-app/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { bugsReducer, spinnerReducer } from '../reducers';
 
 let rootReducer = combineReducers({
@@ -6,6 +6,19 @@ let rootReducer = combineReducers({
 	spinnerData : spinnerReducer
 });
 
-let appStore = createStore(rootReducer);
+function logger({getState, dispatch}){
+	return function(next){
+		return function(action){
+			console.log('old State -> ', getState());
+			console.log('current Action -> ', action);
+			var newState =  next(action);
+			console.log('new State -> ', getState());
+			return newState;
+		}
+	}
+}
+
+
+let appStore = createStore(rootReducer, applyMiddleware(logger));
 
 export default appStore;
